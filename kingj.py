9d361abef1ee6306c59593c50ed3c96c23d034b9
Kingjillu <oijmalde1@gmail.com>
2026-09-07 05:05:30 +0600

    Update kingj.py

--- kingj.py
+++ kingj.py
@@ -43,7 +43,7 @@ def get_machine_key():
     return "ROOH-" + hashlib.md5(raw.encode()).hexdigest().upper()[:12]
 
 def check_approval(key):
-    url = "https://raw.githubusercontent.com/cadetrooh/roohkey/refs/heads/main/keys.txt"
+    url = "https://raw.githubusercontent.com/kingjillu/keysrooh/refs/blob/main/keys.txt"
     try:
         resp = requests.get(url, timeout=10)
         if resp.status_code == 200:
